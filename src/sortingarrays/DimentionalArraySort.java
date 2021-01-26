2da0e4cc1af795296b83f870078c73043601ec84
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-01-26 23:24:04 +0200

    Array block with its members shifting backwards and forwards.

---
+++ src/sortingarrays/DimentionalArraySort.java
@@ -0,0 +1,45 @@
+package sortingarrays;
+
+public class DimentionalArraySort {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int[][] array2d = new int[array.length][array.length];
+        int arraySpan = array2d.length;
+
+//      Array rows shifting backwards:
+        for (int i = 0; i < arraySpan; i++) {
+            for (int j = 0; j < arraySpan; j++) {
+                array2d[i][j] = array[(i + j) % arraySpan];
+            }
+        }
+//      Print the resulting array: 
+        for (int i = 0; i < arraySpan; i++) {
+            int[] row = array2d[i];
+            for (int j = 0; j < row.length; j++) {
+                int element = row[j];
+                System.out.print(element + "");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("");
+        System.out.println("");
+        
+//        Array rows shifting forwards: 
+        for (int i = 0; i < arraySpan; i++) {
+            for (int j = 0; j < arraySpan; j++) {
+                array2d[i][(i + j) % arraySpan] = array[j];
+            }
+        }
+//      Print the resulting array: 
+        for (int i = 0; i < arraySpan; i++) {
+            int[] row = array2d[i];
+            for (int j = 0; j < row.length; j++) {
+                int element = row[j];
+                System.out.print(element + "");
+            }
+            System.out.println("");
+        }
+    }
+}
